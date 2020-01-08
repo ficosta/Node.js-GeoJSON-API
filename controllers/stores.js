b64dfd91b1e22493a95541564df3682afd935baf
Felipe <felipeiasi@gmail.com>
2020-01-08 18:37:31 -0300

    Get Controller Store

--- controllers/stores.js
+++ controllers/stores.js
@@ -1,6 +1,19 @@
+const Store = require('../models/Store');
+
 // @desc Get all stores
 // @route GET /api/v1/stores
 // @access Public
-exports.getStores = (req, res, next) => {
-    res.send('Hello');
+exports.getStores = async (req, res, next) => {
+    try {
+        const stores = await Store.find();
+
+        return res.status(200).json({
+            sucess: true,
+            count: stores.lenght || 0,
+            data: stores
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({errro: 'server error'})
+    }
 };
